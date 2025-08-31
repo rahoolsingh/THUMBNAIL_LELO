ac0d2261672fc57e813e64391fc3f7de19b42279
Veer Rajpoot <rahulksingh3907@gmail.com>
2025-08-31 19:59:32 +0530

    refactor: Simplify CORS configuration and improve code formatting in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -8,24 +8,33 @@ import enhancePrompt from "./routes/enhancePrompt.js";
 import mongoose from "mongoose";
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors({
-    origin: JSON.parse(process.env.CORS_ORIGIN || "[]"),
-    credentials: true,
-}));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 app.use(clerkMiddleware());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.use("/api/v1/generate", requireAuth(), uploadImages, saveImages, enhancePrompt, generateRoutes);
+app.use(
+    "/api/v1/generate",
+    requireAuth(),
+    uploadImages,
+    saveImages,
+    enhancePrompt,
+    generateRoutes
+);
 // MongoDB connection and app listen
 mongoose
     .connect(process.env.MONGODB_URI || "")
     .then(() => {
-    console.log(`Connected to MongoDB`);
-    app.listen(PORT, () => {
-        console.log(`Server is running on port http://localhost:${PORT}`);
-    });
-})
+        console.log(`Connected to MongoDB`);
+        app.listen(PORT, () => {
+            console.log(`Server is running on port http://localhost:${PORT}`);
+        });
+    })
     .catch((error) => {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-});
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+    });
