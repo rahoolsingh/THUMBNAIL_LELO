02aeff0959a00289351f922ceb1d4dda8ecbd894
Veer Rajpoot <rahulksingh3907@gmail.com>
2025-09-23 22:52:30 +0530

    Migrating to Next js

--- server/src/routes/generateRoutes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import generateThumbnail from "../controllers/generateThumbnail.js";
-const generateRoutes = Router();
-generateRoutes.post("/thumbnail", generateThumbnail);
-export default generateRoutes;
--- server/src/types/uploadedFile.d.ts
+++
@@ -1,6 +0,0 @@
-export interface UploadedFile {
-  originalName: string;
-  mimeType: string;
-  fileName: string;
-  url: string;
-}
